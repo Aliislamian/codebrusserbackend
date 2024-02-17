719578f2d5d2aa9dd91be8c3b3e75866fcafec91
Aliislamian <aliislamian123@gmail.com>
2024-02-18 00:58:39 +0500

    chnagesssssssssss

--- routes/products.js
+++ routes/products.js
@@ -108,6 +108,58 @@ router.post('/api/product', upload.array('files', 10), async (req, res) => {
 // // GET request to fetch all data
 
 
+router.get('/api/products-subcategoryId/:subcategoryId', async (req, res) => {
+  try {
+    const categoryId = req.params.subcategoryId; // Get the category ID from the request parameters
+
+    // Find subcategories that belong to the specified category ID
+    const subcategories = await Products.find({ subcategory: categoryId });
+
+    // If no subcategories are found, return an error
+    if (!subcategories || subcategories.length === 0) {
+        return res.status(404).json({
+            success: false,
+            error: true,
+            message: "Subcategories not found for the specified category ID!"
+        });
+    }
+
+    // Return the subcategories associated with the specified category ID
+    return res.status(200).json({
+        success: true,
+        error: false,
+        message: "Subcategories retrieved successfully for the specified category ID!",
+        Product: subcategories
+    });
+} catch (error) {
+    console.error(error);
+    return res.status(500).json({
+        success: false,
+        error: true,
+        message: "Error in retrieving subcategories for the specified category ID!"
+    });
+}
+});
+
+
+router.get('/api/products-id/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const productsdata = await Products.findById(id);
+    res.status(200).json({
+      success: true,
+      message: "Fetched data successfully",
+      data: productsdata,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Error",
+    });
+  }
+});
+
 router.get('/api/products', async (req, res) => {
   try {
     const productsdata = await Products.find();
